142a0375dc556c4d3206e2fe513d2300485d484e
divyesh11 <55645570+divyesh11@users.noreply.github.com>
2026-01-23 19:21:42 +0530

    Feat: Add network configuration to SDK initialization (#6)
    This commit updates the `MedAssistSDK.init()` call in the test app to include a `networkConfig` parameter. This new configuration provides network-related details such as the base URL, app info, and an empty `TokenStorage` implementation.

--- test-app/src/main/java/com/eka/medassist/MainActivity.kt
+++ test-app/src/main/java/com/eka/medassist/MainActivity.kt
@@ -10,11 +10,15 @@ import androidx.activity.viewModels
 import com.eka.conversation.client.models.Environment
 import com.eka.conversation.common.models.UserInfo
 import com.eka.medassist.ui.chat.client.MedAssistSDK
+import com.eka.medassist.ui.chat.client.MedAssistSDK.TAG
+import com.eka.medassist.ui.chat.logger.MedAssistLogger
 import com.eka.medassist.ui.chat.presentation.activities.ActivityParams
 import com.eka.medassist.ui.chat.presentation.activities.ChatScreenActivity
 import com.eka.medassist.ui.chat.presentation.screens.ConversationHistoryScreen
 import com.eka.medassist.ui.chat.presentation.viewmodels.EkaChatViewModel
 import com.eka.medassist.ui.theme.MedAssistTheme
+import com.eka.networking.client.NetworkConfig
+import com.eka.networking.token.TokenStorage
 
 class MainActivity : ComponentActivity() {
     val viewModel by viewModels<EkaChatViewModel>()
@@ -26,7 +30,37 @@ class MainActivity : ComponentActivity() {
             context = this,
             debugMode = true,
             environment = Environment.PROD,
-            agentId = BuildConfig.AGENT_ID
+            agentId = BuildConfig.AGENT_ID,
+            networkConfig = NetworkConfig(
+                appId = "medassist",
+                baseUrl = "https://api.eka.care",
+                appVersionCode = 1,
+                appVersionName = "1",
+                headers = emptyMap(),
+                isDebugApp = true,
+                tokenStorage = object : TokenStorage {
+                    override fun getAccessToken(): String {
+                        MedAssistLogger.d(TAG, "getAccessToken")
+                        return ""
+                    }
+
+                    override fun getRefreshToken(): String {
+                        MedAssistLogger.d(TAG, "getRefreshToken")
+                        return ""
+                    }
+
+                    override fun saveTokens(
+                        accessToken: String,
+                        refreshToken: String
+                    ) {
+                        MedAssistLogger.d(TAG, "saveTokens")
+                    }
+
+                    override fun onSessionExpired() {
+                        MedAssistLogger.d(TAG, "onSessionExpired")
+                    }
+                }
+            )
         )
         setContent {
             MedAssistTheme {
